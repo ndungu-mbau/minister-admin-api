51d1cee38905dfc6c4696c1be2896dc70e8257d8
Nelson <mbau.ndungu@gmail.com>
2021-04-16 14:47:45 +0300

    feat: Route to api/admin endpoint

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ hemera.ready(() => {
     }
   }
 
-  app.use("/api", auth, async (req, res, next) => {
+  app.use("/api/admin", auth, async (req, res, next) => {
       return graphqlHTTP({
         schema,
         graphiql: true,
